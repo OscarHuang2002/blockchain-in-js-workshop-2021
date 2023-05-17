9911fec45ad52f5806a83e50ac87f1e9d0680e52
OscarHuang2002 <132676929+OscarHuang2002@users.noreply.github.com>
2023-05-17 22:32:36 +0800

    Add files via upload

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,39 @@
+import sha256 from 'crypto-js/sha256.js'
+
+export const DIFFICULTY = 2
+
 class Block {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
 
-  */
-  constructor() {}
+  constructor(blockchain, previousHash, height, data) {
+    this.blockchain = blockchain;
+    this.previousHash = previousHash;
+    this.height = height;
+    this.data = data;
+    this.nonce = 0;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return sha256(
+        this.previousHash +
+        this.height +
+        this.data +
+        this.nonce
+    ).toString();
+  }
+
+  isValid() {
+    return (
+        this.hash.substring(0, DIFFICULTY) ===
+        '0'.repeat(DIFFICULTY)
+    );
+  }
+
+  setNonce(nonce) {
+    this.nonce = nonce;
+    this.hash = this.calculateHash();
+  }
 }
-
 export default Block
+
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,22 +1,31 @@
-// Blockchain
 class Blockchain {
-  // 1. 完成构造函数及其参数
-  /* 构造函数需要包含 
-      - 名字
-      - 创世区块
-      - 存储区块的映射
-  */
-  constructor() {}
+  constructor(name) {
+    this.name = name
+    this.blocks = {}
+  }
 
-  // 2. 定义 longestChain 函数
-  /* 
-    返回当前链中最长的区块信息列表
-  */
+  addBlock(block) {
+    if (block.isValid()) {
+      this.blocks[block.hash] = block
+    }
+  }
+
   longestChain() {
+    let maxHeight = Math.max(...Object.values(this.blocks).map(block => block.height))
+    let currentBlock = Object.values(this.blocks).find(block => block.height === maxHeight)
+    let longestChain = []
 
+    while (currentBlock) {
+      longestChain.push(currentBlock)
+      currentBlock = Object.values(this.blocks).find(
+          (block) => block.hash === currentBlock.previousHash
+      )
+    }
 
-    return []
+    return longestChain.reverse()
   }
 }
 
 export default Blockchain
+
+
